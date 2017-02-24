ca735c664142af52c3ec84e5858b71f66bf7ac39
Patrik Valkovic <valkopat@fit.cvut.cz>
2017-02-24 19:52:29 +0100

    App souranded by brackets

--- App.ts
+++ App.ts
@@ -1,17 +1,17 @@
-/// <reference path="node_modules/@types/node/index.d.ts" />
+(function(){
+    let net = require('net');
 
-let net = require('net');
-
-class App {
-    public run(): void {
-        let server = net.createServer({}, function (socket: net.Socket) {
-            console.log("Socket connected");
-        });
-        server.listen(1111, 'localhost', function () {
-            console.log("Server is running");
-        });
+    class App {
+        public run(): void {
+            let server = net.createServer({}, function (socket) {
+                console.log("Socket connected");
+            });
+            server.listen(1111, 'localhost', function () {
+                console.log("Server is running");
+            });
+        }
     }
-}
 
-let app = new App();
-app.run();
+    let app = new App();
+    app.run();
+})();
