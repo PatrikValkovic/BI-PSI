638b4bb5ffdb6a153ddfd450358020114df0559a
Patrik Valkovic <valkopat@fit.cvut.cz>
2017-02-25 18:57:22 +0100

    Added Reader class.

---
+++ src/Reader.ts
@@ -0,0 +1,23 @@
+export class Reader {
+    private callback: Function;
+
+    public buffer: string;
+
+    public constructor() {
+        this.buffer = '';
+        this.setCallback(function () {
+        });
+    }
+
+    public setCallback(callback: Function) {
+        this.callback = callback;
+        if(this.buffer.length !== 0)
+            this.callback();
+    }
+
+    public appendText(text : string)
+    {
+        this.appendText(text);
+        this.callback();
+    }
+}
