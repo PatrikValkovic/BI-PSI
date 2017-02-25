47669b7ac82363d857018baa1b50833e52c6aeb4
Patrik Valkovic <valkopat@fit.cvut.cz>
2017-02-25 20:24:54 +0100

    Readed now automatically parse command and control length of acepted string.

--- src/Reader.ts
+++ src/Reader.ts
@@ -1,7 +1,10 @@
+import {Errors} from './Constants';
+
 export class Reader {
     private callback: Function;
 
     public buffer: string;
+    public maxLength: number;
 
     public constructor() {
         this.buffer = '';
@@ -20,6 +23,20 @@ export class Reader {
         console.log("Text obtained, current buffer: " + this.buffer.replace(/\r/g, '\\r')
                         .replace(/\n/g, '\\n')
                         .replace(/\0/g, '\\0'));
-        this.callback();
+
+        let posOfDelimiter = this.buffer.indexOf('\r\n');
+        if (posOfDelimiter < 0) //not accepted whole name yet
+        {
+            if (this.buffer.length > this.maxLength) //already arrive more symbols that require
+                this.callback(Errors.overLength);
+            return;
+        }
+
+        let parsed = this.buffer.substring(0, posOfDelimiter);
+        if (parsed.length > this.maxLength)
+            this.callback(Errors.overLength);
+
+        this.buffer = this.buffer.substring(parsed.length + 2);
+        this.callback(parsed);
     }
 }
