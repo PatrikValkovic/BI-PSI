769afb7584c538b90ee3c0b8c986b81e34e5aad3
Patrik Valkovic <valkopat@fit.cvut.cz>
2017-02-25 11:11:12 +0100

    Added exceptions

---
+++ src/Exceptions.ts
@@ -0,0 +1,20 @@
+export class Exception {
+
+}
+
+export class TimeoutException extends Exception {
+
+}
+
+export class LoginException extends Exception {
+
+}
+
+export class SynaxException extends Exception {
+
+}
+
+export class LogicException extends Exception {
+
+}
+
