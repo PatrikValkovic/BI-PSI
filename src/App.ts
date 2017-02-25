f61e245fb60c593d100c9d8a47fb5f133d3f3a03
Patrik Valkovic <valkopat@fit.cvut.cz>
2017-02-25 18:22:37 +0100

    Included async library and used it in app.

--- src/App.ts
+++ src/App.ts
@@ -1,3 +1,4 @@
+import * as async from 'async/series';
 import * as net from 'net';
 import {Client} from './Client';
 import * as ex from "./Exceptions";
@@ -5,24 +6,30 @@ import * as ex from "./Exceptions";
 class App {
     public run(): void {
         let server = net.createServer({}, function (socket) {
-            try {
-                console.log("Socket connected");
-                let c: Client = new Client(socket);
-                c.authenticate();
-            }
-            catch (e) {
-                switch(e.getType()){
-                    case ex.LOGIC:
-                        break;
-                    case ex.SYNTAX:
-                        break;
-                    case ex.TIMEOUT:
-                        break;
-                    case ex.LOGIN:
-                        break;
+            console.log("Socket connected");
+            let c: Client = new Client(socket);
+
+            async.series([function (callback: Function) {
+                c.authenticate(callback);
+            }], function (err, res) {
+                if (err) {
+                    switch (err) {
+                        case ex.LOGIC:
+                            break;
+                        case ex.SYNTAX:
+                            break;
+                        case ex.TIMEOUT:
+                            socket.end();
+                            socket.destroy();
+                            console.log("Socket deleted because of timeout");
+                        case ex.LOGIN:
+                            break;
+                    }
                 }
-            }
+            });
         });
+
+
         server.listen(1111, 'localhost', function () {
             console.log("Server is running on localhost:1111");
         });
--- src/Client.ts
+++ src/Client.ts
@@ -1,32 +1,21 @@
 import * as net from 'net';
+import * as exception from './Exceptions';
 import {CommunicationFacade} from './CommunicationFacade';
 
 export class Client {
     private socket: net.Socket;
     private timeout;
 
-    public timeoutCreate() {
-        let _this = this;
-        return function () {
-            _this.socket.end();
-            _this.socket.destroy();
-        };
-    }
-
-    public readCreate() {
-        let _this = this;
-        return function(data : string) {
-            console.log('Readed: ' + data);
-        }
-    }
-
     public constructor(socket: net.Socket) {
         this.socket = socket;
-        this.socket.on('data',this.readCreate());
-        this.timeout = setTimeout(this.timeoutCreate(), 1000);
     }
 
-    public authenticate() {
+    public authenticate(callback) {
+
+        this.timeout = setTimeout(function () {
+            callback(exception.TIMEOUT, null);
+        }, 1000);
+
         CommunicationFacade.ServerUser(this.socket);
     }
 }
