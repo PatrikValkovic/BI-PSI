1572511dbc382398357edfd6ab7de3efa1d4e407
Patrik Valkovic <valkopat@fit.cvut.cz>
2017-02-25 11:36:54 +0100

    Added timeout into Client.

--- src/Client.ts
+++ src/Client.ts
@@ -1,18 +1,19 @@
 import * as net from 'net';
 
-
 export class Client {
     private socket: net.Socket;
     private timeout;
 
-    private timeouted(): void {
-
+    public timeoutCreate() {
+        let _this = this;
+        return function () {
+            _this.socket.end();
+            _this.socket.destroy();
+        };
     }
 
     public constructor(socket: net.Socket) {
         this.socket = socket;
-        this.timeout = setTimeout(this.timeouted, 1000);
+        this.timeout = setTimeout(this.timeoutCreate(), 1000);
     }
-
-
 }
