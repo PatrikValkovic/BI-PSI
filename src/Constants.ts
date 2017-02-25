ade1fd3e27a334757682f72e9058373629f92276
Patrik Valkovic <valkopat@fit.cvut.cz>
2017-02-25 20:12:50 +0100

    Added constant file and fill it with constants.

---
+++ src/Constants.ts
@@ -0,0 +1,15 @@
+export const OverLength = 1;
+
+export const Direction = {
+    up: 1,
+    right: 2,
+    down: 4,
+    left: 8,
+};
+
+export const Errors = {
+    overLength: 1,
+    timeout: 2,
+    login: 4,
+    syntax: 8,
+};
