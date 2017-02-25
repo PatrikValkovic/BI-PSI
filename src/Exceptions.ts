f486092e5d3e72ea72762fe5b674d092ee5460f2
Patrik Valkovic <valkopat@fit.cvut.cz>
2017-02-25 11:18:18 +0100

    Added types to exceptions

--- src/Exceptions.ts
+++ src/Exceptions.ts
@@ -1,20 +1,19 @@
 export class Exception {
-
 }
 
 export class TimeoutException extends Exception {
-
+    private type = 'timeout';
 }
 
 export class LoginException extends Exception {
-
+    private type = 'login';
 }
 
-export class SynaxException extends Exception {
-
+export class SyntaxException extends Exception {
+    private type = 'syntax';
 }
 
 export class LogicException extends Exception {
-
+    private type = 'logic';
 }
 
