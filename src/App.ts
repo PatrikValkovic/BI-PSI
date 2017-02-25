3b189bb77d17274d152c9f999fd5d1c1d255979f
Patrik Valkovic <valkopat@fit.cvut.cz>
2017-02-25 11:36:09 +0100

    Added base exception handling into App class.

--- src/App.ts
+++ src/App.ts
@@ -1,14 +1,29 @@
 import * as net from 'net';
 import {Client} from './Client';
+import * as ex from "./Exceptions";
 
 class App {
     public run(): void {
         let server = net.createServer({}, function (socket) {
-            let c: Client = new Client(socket);
-            console.log("Socket connected");
+            try {
+                console.log("Socket connected");
+                let c: Client = new Client(socket);
+            }
+            catch (e) {
+                switch(e.getType()){
+                    case ex.LOGIC:
+                        break;
+                    case ex.SYNTAX:
+                        break;
+                    case ex.TIMEOUT:
+                        break;
+                    case ex.LOGIN:
+                        break;
+                }
+            }
         });
         server.listen(1111, 'localhost', function () {
-            console.log("Server is running");
+            console.log("Server is running on localhost:1111");
         });
     }
 }
