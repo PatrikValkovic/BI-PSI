c5d77b957982b18f4827bfbc57a8f416d140caf5
Patrik Valkovič <patrik.valkovic@hotmail.cz>
2017-03-04 19:51:49 +0100

    Implemented init connection for downloader

--- second/Downloader.cs
+++ second/Downloader.cs
@@ -12,10 +12,35 @@ namespace second
     {
         private StreamWriter outFile;
         private Socket socket;
+
+        private UInt32 connectionNumber;
+        private UInt16 serialNumber;
+        private UInt16 confirmationNumber;
+        private byte[] data;
+
         public Downloader(Socket s, StreamWriter writer)
         {
             this.outFile = writer;
             this.socket = s;
+        }
+
+        public void initConnection()
+        {
+            int i = 0;
+            this.socket.ReceiveTimeout = 100;
+
+            byte flags;
+            while(i<20)
+            {
+                CommunicationFacade.Send(this.socket, 0, 0, 0, (byte)Flag.SYN, new byte[] { (byte)Command.DOWNLOAD });
+                CommunicationFacade.Receive(this.socket, out this.connectionNumber, out this.serialNumber, out this.confirmationNumber, out flags, out data);
+                if(flags == (byte)Flag.SYN)
+                {
+                    Console.WriteLine($"Connection established - communication {this.connectionNumber}");
+                    break;
+                }
+            }
+            this.socket.ReceiveTimeout = 0;
         }
     }
 }
