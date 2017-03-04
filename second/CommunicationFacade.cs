d5c4401f57e205d893736863c5fa0b0c69eeea8d
Patrik Valkovič <patrik.valkovic@hotmail.cz>
2017-03-04 14:53:55 +0100

    Implemented send of sockets from facade

--- second/CommunicationFacade.cs
+++ second/CommunicationFacade.cs
@@ -11,15 +11,29 @@ namespace second
     {
         private static readonly int MAXIMUM_LENGTH_OF_MESSAGE = 264;
 
-        private static byte[] buffer = new byte[MAXIMUM_LENGTH_OF_MESSAGE];
+        private static byte[] inputBuffer = new byte[MAXIMUM_LENGTH_OF_MESSAGE];
         public static void receive(Socket socket, out UInt32 ConnectionNumber, out UInt16 SerialNumber, out UInt16 ConfirmationNumber, out byte Flags, out byte[] Data)
         {
-            socket.Receive(buffer);
-            ConnectionNumber = BitConverter.ToUInt32(buffer,0);
-            SerialNumber = BitConverter.ToUInt16(buffer, 4);
-            ConfirmationNumber = BitConverter.ToUInt16(buffer, 6);
-            Flags = buffer[8];
-            Data = buffer.Skip(8).ToArray();
+            socket.Receive(inputBuffer);
+            ConnectionNumber = BitConverter.ToUInt32(inputBuffer,0);
+            SerialNumber = BitConverter.ToUInt16(inputBuffer, 4);
+            ConfirmationNumber = BitConverter.ToUInt16(inputBuffer, 6);
+            Flags = inputBuffer[8];
+            Data = inputBuffer.Skip(8).ToArray();
+        }
+
+        private static byte[] outBuffer = new byte[MAXIMUM_LENGTH_OF_MESSAGE];
+        public static void send(Socket socket, UInt32 ConnectionNumber, UInt16 SerialNumber, UInt16 ConfirmationNumber, byte Flags, byte[] Data)
+        {
+            if (Data.Length > 255)
+                throw new ArgumentException("Data have more then 255 bytes");
+
+            BitConverter.GetBytes(ConnectionNumber).CopyTo(outBuffer,0);
+            BitConverter.GetBytes(SerialNumber).CopyTo(outBuffer,4);
+            BitConverter.GetBytes(ConnectionNumber).CopyTo(outBuffer,6);
+            outBuffer[8] = Flags;
+            Data.CopyTo(outBuffer, 9);
+            socket.Send(outBuffer.Take(Data.Length + 9).ToArray());
         }
     }
 }
