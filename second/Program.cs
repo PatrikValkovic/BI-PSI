80839eb89aa63cbda1920882007f461ef88ff45d
Patrik Valkovič <patrik.valkovic@hotmail.cz>
2017-03-04 14:30:37 +0100

    Implemented switch for actions

--- second/Program.cs
+++ second/Program.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Net.Sockets;
+using System.Net;
 using System.Threading;
+using System.IO;
 
 namespace second
 {
@@ -16,11 +18,39 @@ namespace second
             int port = 4000;
             string file = "";
 
-            if (args.Length == 0)
+            if (args.Length == 0 || args.Length > 2)
             {
                 Console.WriteLine("Usage: second <ip> [<file>]");
                 Console.WriteLine($"Default action will be used: download form ip {ip}");
                 args = new string[] { ip };
+            }
+
+            //ip validation
+            ip = args[0];
+            IPAddress address;
+            if (!IPAddress.TryParse(ip, out address))
+            {
+                Console.WriteLine($"Invalid IP address {ip}");
+                Console.WriteLine("Program will be terminated");
+                return;
+            };
+
+            if (args.Length == 1)
+            {
+                Console.WriteLine("Download action will be use");
+                //TODO download
+            }
+            else if (args.Length == 2)
+            {
+                Console.WriteLine("Firmware upload action will be use");
+                //file validation
+                if(!File.Exists(args[1]))
+                {
+                    Console.WriteLine($"Invalid file {args[1]}");
+                    Console.WriteLine("Program will be terminated");
+                    return;
+                }
+                //TODO upload
             }
             byte[] buffer = new byte[10];
             using (Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp))
