751466a3accae08cbcbdaa2e8f5462c0a8618122
Patrik Valkovič <patrik.valkovic@hotmail.cz>
2017-03-04 20:58:41 +0100

    Exceptions moved into separate folder and namespace

--- second/Exceptions/MaximumAttempException.cs
+++ second/Exceptions/MaximumAttempException.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace second
+namespace second.Exceptions
 {
-    class TerminateException : Exception
+    class MaximumAttempException : Exception
     {
     }
 }
