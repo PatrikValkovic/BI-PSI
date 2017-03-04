1f00d2684a773f9e51b034fcdd84cc1a5c81d46f
Patrik Valkovič <patrik.valkovic@hotmail.cz>
2017-03-04 19:40:22 +0100

    Added constants for flags

---
+++ second/Constants.cs
@@ -0,0 +1,15 @@
+﻿namespace second
+{
+    enum Command : byte
+    {
+        DOWNLOAD = 0x1,
+        UPLOAD = 0x2
+    }
+
+    enum Flag : byte
+    {
+        RST = 0x1,
+        FIN = 0x2,
+        SYN = 0x4
+    }
+}
