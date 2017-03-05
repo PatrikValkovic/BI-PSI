54830d1ad1f61602f0f647fac99737255cf35be6
Patrik Valkovič <patrik.valkovic@hotmail.cz>
2017-03-05 16:59:36 +0100

    Created classes for BasePacket and DownloadedPacket

---
+++ second/Packets/BasePacket.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace second.Packets
+{
+    class BasePacket
+    {
+        public BasePacket(byte[] data, UInt32 connectionNumber, byte flags)
+        {
+            this.Data = data;
+            this.ConnectionNumber = connectionNumber;
+            this.Flags = flags;
+        }
+        public readonly byte[] Data;
+        public readonly UInt32 ConnectionNumber;
+        public readonly byte Flags;
+    }
+}
---
+++ second/Packets/DownloadPacket.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace second.Packets
+{
+    class DownloadPacket : BasePacket
+    {
+        public DownloadPacket(byte[] data,UInt32 connectionNumber, byte flags, UInt16 serialNumber)
+            :base(data,connectionNumber,flags)
+        {
+            this.SerialNumber = serialNumber;
+        }
+
+        public readonly UInt16 SerialNumber;
+    }
+}
