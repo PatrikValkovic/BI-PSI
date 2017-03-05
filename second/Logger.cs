30b6f8bd38a5437ec578edb6c57e9723e335292f
Patrik Valkovič <patrik.valkovic@hotmail.cz>
2017-03-05 17:04:41 +0100

    Added Loger class that lock Console for output

---
+++ second/Logger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace second
+{
+    class Logger
+    {
+        private static object locker = new object();
+
+        public static void WriteLine(string text)
+        {
+            lock (locker)
+            {
+                Console.WriteLine(text);
+            }
+        }
+    }
+}
