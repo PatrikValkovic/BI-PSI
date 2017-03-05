235bc8ecec86fd175a4137b005811ed3e03f1da5
Patrik Valkovič <patrik.valkovic@hotmail.cz>
2017-03-05 16:43:47 +0100

    Thread moved from Downloader to Uploader, because threads are not required in Downloader

--- second/Downloader.cs
+++ second/Downloader.cs
@@ -27,34 +27,9 @@ namespace second
             CommunicationFacade.InitConnection(this.socket,out this.connectionNumber,Command.DOWNLOAD);
         }
 
-        static private void TimeoutCheckerThread(object Param)
-        {
-            Console.WriteLine("TimeoutChecker thread started");
-        }
-
-        static private void ProccessDataThread(object Param)
-        {
-            Console.WriteLine("ProccessData thread started");
-        }
-
-        static private void ReceiveThread(object Param)
-        {
-            Console.WriteLine("Receive thread started");
-        }
-
         public void AcceptFile()
         {
-            Thread timeoutChecker = new Thread(TimeoutCheckerThread);
-            Thread receiver = new Thread(ReceiveThread);
-            Thread processData = new Thread(ProccessDataThread);
-
-            receiver.Start(new object());
-            timeoutChecker.Start(new object());
-            processData.Start(new object());
-
-            receiver.Join();
-            timeoutChecker.Join() ;
-            processData.Join();
+            
         }
     }
 }
--- second/Uploader.cs
+++ second/Uploader.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace second
@@ -16,6 +17,36 @@ namespace second
         {
             this.inFile = reader;
             this.socket = s;
+        }
+
+        static private void TimeoutCheckerThread(object Param)
+        {
+            Console.WriteLine("TimeoutChecker thread started");
+        }
+
+        static private void ProccessDataThread(object Param)
+        {
+            Console.WriteLine("ProccessData thread started");
+        }
+
+        static private void ReceiveThread(object Param)
+        {
+            Console.WriteLine("Receive thread started");
+        }
+
+        public void SendFile()
+        {
+            Thread timeoutChecker = new Thread(TimeoutCheckerThread);
+            Thread receiver = new Thread(ReceiveThread);
+            Thread processData = new Thread(ProccessDataThread);
+
+            receiver.Start(new object());
+            timeoutChecker.Start(new object());
+            processData.Start(new object());
+
+            receiver.Join();
+            timeoutChecker.Join();
+            processData.Join();
         }
     }
 }
