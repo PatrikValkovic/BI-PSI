34fc74256f7a362591889c6000a26ef1ae07031a
Patrik Valkovič <patrik.valkovic@hotmail.cz>
2017-03-05 16:47:43 +0100

    Added Sizes constants

--- second/Constants.cs
+++ second/Constants.cs
@@ -12,4 +12,14 @@
         FIN = 0x2,
         SYN = 0x4
     }
+
+    enum Sizes : int
+    {
+        PACKET_MIN = 9,
+        PACKET_MAX = 264,
+        TIMEOUT = 100,
+        MAX_DATA = 255,
+        WINDOW_SIZE = 2040,
+        WINDOW_PACKETS = WINDOW_SIZE / MAX_DATA
+    }
 }
