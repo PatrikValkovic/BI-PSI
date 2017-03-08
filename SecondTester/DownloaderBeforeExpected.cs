19166dc89cda27c90c647ae4874333f56af0aa77
Patrik Valkovič <patrik.valkovic@hotmail.cz>
2017-03-08 22:55:05 +0100

    Added test, when min and max are in direct interval for second edge

--- SecondTester/DownloaderBeforeExpected.cs
+++ SecondTester/DownloaderBeforeExpected.cs
@@ -43,7 +43,7 @@ namespace SecondTester
             Assert.AreEqual((UInt64)66300, down.SerialNumber);
         }
 
-        [TestMethod]                    //                   V
+        [TestMethod]                     //                   V
         public void At66045Arrive2549()  //  .......MIN------MAX.....
         {
             Downloader d = new Downloader(null, new BinaryWriter(new MemoryStream()));
@@ -63,6 +63,70 @@ namespace SecondTester
             object res = o.Invoke("receive", new CommunicationPacket(0, 2805, 0, 0, new byte[] { }));
             DownloadPacket down = (DownloadPacket)res;
             Assert.AreEqual((UInt64)2805, down.SerialNumber); //it thinks, that it overlaps
+        }
+
+
+
+
+
+        ///// NEXT EDGE
+
+
+
+
+        [TestMethod]
+        public void At131580Arrive253() // ...P....MIN-------MAX.....
+        {
+            Downloader d = new Downloader(null, new BinaryWriter(new MemoryStream()));
+            PrivateObject o = new PrivateObject(d);
+            o.SetFieldOrProperty("required", (UInt64)131580);
+            object res = o.Invoke("receive", new CommunicationPacket(0, 253, 0, 0, new byte[] { }));
+            DownloadPacket down = (DownloadPacket)res;
+            Assert.AreEqual((UInt64)131325, down.SerialNumber);
+        }
+
+        [TestMethod]                    //          V
+        public void At131580Arrive508()  //  .......MIN------MAX.....
+        {
+            Downloader d = new Downloader(null, new BinaryWriter(new MemoryStream()));
+            PrivateObject o = new PrivateObject(d);
+            o.SetFieldOrProperty("required", (UInt64)131580);
+            object res = o.Invoke("receive", new CommunicationPacket(0, 508, 0, 0, new byte[] { }));
+            DownloadPacket down = (DownloadPacket)res;
+            Assert.AreEqual((UInt64)131580, down.SerialNumber);
+        }
+
+        [TestMethod]
+        public void At131580Arrive1018()  //  .......MIN--P---MAX.....
+        {
+            Downloader d = new Downloader(null, new BinaryWriter(new MemoryStream()));
+            PrivateObject o = new PrivateObject(d);
+            o.SetFieldOrProperty("required", (UInt64)131580);
+            object res = o.Invoke("receive", new CommunicationPacket(0, 1018, 0, 0, new byte[] { }));
+            DownloadPacket down = (DownloadPacket)res;
+            Assert.AreEqual((UInt64)132090, down.SerialNumber);
+        }
+
+        [TestMethod]                      //                   V
+        public void At131580Arrive2548()  //  .......MIN------MAX.....
+        {
+            Downloader d = new Downloader(null, new BinaryWriter(new MemoryStream()));
+            PrivateObject o = new PrivateObject(d);
+            o.SetFieldOrProperty("required", (UInt64)131580);
+            object res = o.Invoke("receive", new CommunicationPacket(0, 2548, 0, 0, new byte[] { }));
+            DownloadPacket down = (DownloadPacket)res;
+            Assert.AreEqual((UInt64)133620, down.SerialNumber);
+        }
+
+        [TestMethod]
+        public void At131580Arrive3059()  //  .......MIN------MAX...P..
+        {
+            Downloader d = new Downloader(null, new BinaryWriter(new MemoryStream()));
+            PrivateObject o = new PrivateObject(d);
+            o.SetFieldOrProperty("required", (UInt64)131580);
+            object res = o.Invoke("receive", new CommunicationPacket(0, 3059, 0, 0, new byte[] { }));
+            DownloadPacket down = (DownloadPacket)res;
+            Assert.AreEqual((UInt64)68595, down.SerialNumber); //it thinks, that it overlaps
         }
     }
 }
