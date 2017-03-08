6c09cfc152fe05aa4651e8983d84355c806896de
Patrik Valkovič <patrik.valkovic@hotmail.cz>
2017-03-08 17:19:30 +0100

    Writed test to compute over edge of required

--- SecondTester/UnitTest1.cs
+++ SecondTester/UnitTest1.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using second;
+using System.Net.Sockets;
+using System.IO;
+using second.Packets;
 
 namespace SecondTester
 {
@@ -7,8 +11,25 @@ namespace SecondTester
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void SerialBeforeEdgeOfOverflow()
         {
+            Downloader d = new Downloader(null,new StringWriter());
+            PrivateObject o = new PrivateObject(d);
+            o.SetFieldOrProperty("required", (UInt64)UInt16.MaxValue-510);
+            object res = o.Invoke("receive", new CommunicationPacket(0,UInt16.MaxValue-255,0,0,new byte[] { }));
+            DownloadPacket down = (DownloadPacket)res;
+            Assert.AreEqual(down.SerialNumber,(UInt64) UInt16.MaxValue-255);
+        }
+
+        [TestMethod]
+        public void SerialAfterEdgeOfOverflow()
+        {
+            Downloader d = new Downloader(null, new StringWriter());
+            PrivateObject o = new PrivateObject(d);
+            o.SetFieldOrProperty("required", (UInt64)UInt16.MaxValue - 510);
+            object res = o.Invoke("receive", new CommunicationPacket(0, 255, 0, 0, new byte[] { }));
+            DownloadPacket down = (DownloadPacket)res;
+            Assert.AreEqual(down.SerialNumber, (UInt64)UInt16.MaxValue + 255);
         }
     }
 }
